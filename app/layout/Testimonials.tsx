88d72d9bab0cc10a8b33f4b914298c0b216bd787
harshkaranjiya10 <harshkaranjiya10@gmail.com>
2025-06-09 12:47:11 +0530

    Testemonials

--- app/layout/Testimonials.tsx
+++ app/layout/Testimonials.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 export default function Testimonials() {
   const TestimonialList = [
     {
-      name: "John Doe",
-      company: "ABC Company",
+      name: "Jamie Lee",
+      company: "Pulse",
       message:
-        "“Our teams productivity has increased significantly since we started using this tool“",
+        "“These AI tools have completely revolutionized our SEO entire strategy overnight“",
     },
     {
       name: "John Doe",
@@ -54,22 +54,22 @@ export default function Testimonials() {
                     {index}
                   </div>
                   <div className="flex gap-3 items-center mt-5">
-                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[#8C45F4] after:mix-blend-soft-light before:absolute before:content-[''] before:inset-0 before:border-white/30 before:z-10 before:rounded-lg">
-                    <Image
-                      src="/vercel.svg"
-                      width={4}
-                      height={4}
-                      alt={`Avatar for ${testimonial.name}`}
-                      className="w-11 h-11 rounded-lg grayscale"
-                    />
-                  </div>
-                  <div>
-                    <div>{testimonial.name}</div>
-                    <div className="text-white/50 text-sm">
-                      {testimonial.company}
+                    <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[#8C45F4] after:mix-blend-soft-light before:absolute before:content-[''] before:inset-0 before:border-white/30 before:z-10 before:rounded-lg">
+                      <Image
+                        src="/vercel.svg"
+                        width={4}
+                        height={4}
+                        alt={`Avatar for ${testimonial.name}`}
+                        className="w-11 h-11 rounded-lg grayscale"
+                      />
+                    </div>
+                    <div>
+                      <div>{testimonial.name}</div>
+                      <div className="text-white/50 text-sm">
+                        {testimonial.company}
+                      </div>
                     </div>
                   </div>
-                </div>
                 </div>
               )
             )}
